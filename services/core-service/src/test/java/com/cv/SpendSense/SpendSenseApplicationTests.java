05d1e4c2881e8780000bd6f3fef2346cdd3e0b7b
omkar <omkarpadalkar21@gmail.com>
2026-03-03 22:04:04 +0530

    Init: Create folder structure

---
+++ services/core-service/src/test/java/com/cv/SpendSense/SpendSenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cv.SpendSense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendSenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
